2e10593f30eef9bfbb6dd016d297eec00b4e37ba
Nora <captainmetalmighty@gmail.com>
2022-01-13 23:55:04 +0100

    Implement basic user images retrieval

--- src/TwikturesClient/Data/TwitterApiService.cs
+++ src/TwikturesClient/Data/TwitterApiService.cs
@@ -15,8 +15,8 @@ namespace TwikturesClient.Data
             if (uri != null)
             {
                 _backendUri = uri;
-                if (!_backendUri.EndsWith('/'))
-                    _backendUri = _backendUri + '/';
+                if (_backendUri.EndsWith('/'))
+                    _backendUri.Trim('/');
             }
             else
                 throw new InvalidOperationException($"The environment variable {uriEnvName} hasn't been ");
@@ -24,12 +24,35 @@ namespace TwikturesClient.Data
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             _httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-            _httpClient.BaseAddress = new Uri($"http://{_backendUri}");
+            //_httpClient.BaseAddress = new Uri(_backendUri);
         }
 
         public async Task<User> GetUserAsync(string username)
         {
-            return await _httpClient.GetFromJsonAsync<User>($"twitter/user/{username}");
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<User>($"{_backendUri}/api/twitter/user/{username}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        public async Task<List<Tweet>> GetUserTweetsAsync(string username)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Tweet>>($"{_backendUri}/api/twitter/user/{username}/tweets");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
         }
     }
 }

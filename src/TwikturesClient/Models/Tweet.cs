7c6e45bd5cdae5838784d274fa8fa0ae94066d7d
Nora <captainmetalmighty@gmail.com>
2022-01-13 23:51:02 +0100

    Update models with the ones from API

--- src/TwikturesClient/Models/Tweet.cs
+++ src/TwikturesClient/Models/Tweet.cs
@@ -2,5 +2,9 @@
 {
     public class Tweet
     {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public List<string> ImageURIs { get; set; }
     }
 }
--- src/TwikturesClient/Models/User.cs
+++ src/TwikturesClient/Models/User.cs
@@ -2,11 +2,10 @@
 {
     public class User
     {
-        public string Url { get; set; }
-        public string UserName { get; set; }
-        public string ProfileImageUrl { get; set; }
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Name { get; set; }
-
+        public string Url { get; set; }
+        public string Username { get; set; }
+        public string AvatarUrl { get; set; }
     }
 }

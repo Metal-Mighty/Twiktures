d754d06b45b7c134eb5d2a9d55507369e4580972
Nora Emma "Metal-Mighty" Barlow <5247527+Metal-Mighty@users.noreply.github.com>
2022-04-11 19:48:39 +0200

    Add basic CI (#8)
    * Add .gitignore
    
    * Add backend API project
    
    * Add TwikturesClient Blazor client app project
    
    * Moving projects to a single solution
    
    * Configure Secret Detection in `.gitlab-ci.yml`, creating this file if it does not already exist
    
    * Basic Twitter communication and media display (#1)
    
    * Add basic endpoints using Tweetinvi
    
    * Add basic model for Twitter objects in frontend
    
    * WIP get user info from Twitter
    
    * Fix API routing
    
    * Get large 400x400 profile image for user in API
    
    * Update models with the ones from API
    
    * Sample client cleanup
    
    * Add docker-compose for api
    
    * Implement basic user images retrieval
    
    * Add pagination
    
    * Update launch and appsettings
    
    * Add query logging and disable https redirection
    
    * Add Circle-CI base file
    
    * Edit image name for executor
    
    * Use base image from microsoft registry
    
    * Set working directory to ./src
    
    * Separate build and test jobs
    
    * Change workflow name
    
    * Enable tests job
    
    * Add Codecov upload
    
    * Add Codecov upload
    
    * Fix yaml indentation
    
    * Change Codecov version, fix step
    
    * Disable Codecov for now
    
    * Remove gitlab ci
    
    * Add SonarCloud scanning
    
    * Fix export command
    
    * Fix env var syntax
    
    * Fix env var syntax
    
    * Add working directory

---
+++ src/TwikturesApi/Usings.cs
@@ -0,0 +1,10 @@
+﻿// Framework
+global using Microsoft.AspNetCore.Mvc;
+
+// Internal
+global using TwikturesApi.Models;
+
+// External
+global using Tweetinvi;
+global using Tweetinvi.Models.V2;
+global using Tweetinvi.Parameters.V2;
---
+++ src/TwikturesClient/Models/Tweet.cs
@@ -0,0 +1,10 @@
+﻿namespace TwikturesClient.Models
+{
+    public class Tweet
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public List<string> ImageURIs { get; set; }
+    }
+}

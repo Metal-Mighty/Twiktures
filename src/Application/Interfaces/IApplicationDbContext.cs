e6044db25ec96818e7006a8f47e32d12c9757ae4
Nora Emma Metal-Mighty Barlow <captainmetalmighty@gmail.com>
2022-07-16 10:00:09 +0200

    WIP Implement EntityFramework + rework architecture

---
+++ src/Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Interfaces;
+
+internal interface IApplicationDbContext
+{
+    DbSet<TweetEntity> Tweets { get; set; }
+    DbSet<TwitterUserEntity> TwitterUsers { get; set; }
+    DbSet<TwiktureUserEntity> TwiktureUsers { get; set; }
+
+    Task<uint> SaveAsync();
+}
---
+++ src/Domain/Entity/TwitterUserEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Entity;
+
+public class TwitterUserEntity : BaseEntity<uint>
+{
+
+}
